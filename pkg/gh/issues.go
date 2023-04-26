5f086b8bff52d3307e2870789bce028c023a2ec5
chand1012 <chandler@multisiglabs.org>
2023-04-25 22:56:58 -0400

    Add additional GH functions

---
+++ pkg/gh/issues.go
@@ -0,0 +1,93 @@
+package gh
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/chand1012/ottodocs/pkg/config"
+)
+
+func GetIssue(owner, repo string, issueNumber int, conf *config.Config) (*IssueWithComments, error) {
+	if conf.GHToken == "" {
+		return nil, fmt.Errorf("no GitHub token found")
+	}
+
+	issueURL := fmt.Sprintf("https://api.github.com/repos/%s/%s/issues/%d", owner, repo, issueNumber)
+	commentsURL := fmt.Sprintf("https://api.github.com/repos/%s/%s/issues/%d/comments", owner, repo, issueNumber)
+
+	issueReq, err := http.NewRequest("GET", issueURL, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	commentsReq, err := http.NewRequest("GET", commentsURL, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	issueReq.Header.Set("Authorization", fmt.Sprintf("Bearer %s", conf.GHToken))
+	commentsReq.Header.Set("Authorization", fmt.Sprintf("Bearer %s", conf.GHToken))
+
+	client := &http.Client{}
+
+	issueResp, err := client.Do(issueReq)
+	if err != nil {
+		return nil, err
+	}
+	defer issueResp.Body.Close()
+
+	commentsResp, err := client.Do(commentsReq)
+	if err != nil {
+		return nil, err
+	}
+	defer commentsResp.Body.Close()
+
+	if issueResp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("failed to retrieve issue: %s", issueResp.Status)
+	}
+
+	if commentsResp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("failed to retrieve issue comments: %s", commentsResp.Status)
+	}
+
+	var issue Issue
+	err = json.NewDecoder(issueResp.Body).Decode(&issue)
+	if err != nil {
+		return nil, err
+	}
+
+	var comments []Comment
+	err = json.NewDecoder(commentsResp.Body).Decode(&comments)
+	if err != nil {
+		return nil, err
+	}
+
+	issueWithComments := &IssueWithComments{
+		Issue:    issue,
+		Comments: comments,
+	}
+
+	return issueWithComments, nil
+}
+
+type Issue struct {
+	Number    int    `json:"number"`
+	Title     string `json:"title"`
+	Body      string `json:"body"`
+	State     string `json:"state"`
+	CreatedAt string `json:"created_at"`
+	UpdatedAt string `json:"updated_at"`
+}
+
+type IssueWithComments struct {
+	Issue    Issue     `json:"issue"`
+	Comments []Comment `json:"comments"`
+}
+
+type Comment struct {
+	ID        int    `json:"id"`
+	Body      string `json:"body"`
+	CreatedAt string `json:"created_at"`
+	UpdatedAt string `json:"updated_at"`
+}
--- pkg/gh/pr.go
+++ pkg/gh/pr.go
@@ -58,3 +58,68 @@ func OpenPullRequest(data map[string]string, owner string, repo string, conf *co
 	// Return the pull request number and nil error
 	return pr.Number, nil
 }
+
+func SubmitPullRequestReview(owner, repo string, pullRequestNumber int, review *PullRequestReview, conf *config.Config) error {
+	if conf.GHToken == "" {
+		return fmt.Errorf("no GitHub token found")
+	}
+
+	url := fmt.Sprintf("https://api.github.com/repos/%s/%s/pulls/%d/reviews", owner, repo, pullRequestNumber)
+
+	payload, err := json.Marshal(review)
+	if err != nil {
+		return err
+	}
+
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(payload))
+	if err != nil {
+		return err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", conf.GHToken))
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("failed to submit review: %s", resp.Status)
+	}
+
+	return nil
+}
+
+type PullRequestReview struct {
+	// The body of the review. This is optional.
+	Body string `json:"body,omitempty"`
+	// The event to perform on the pull request. This can be one of:
+	// "APPROVE", "REQUEST_CHANGES", "COMMENT", or "DISMISS".
+	Event string `json:"event,omitempty"`
+	// An array of comments to add to the review. This is optional.
+	Comments []*ReviewComment `json:"comments,omitempty"`
+	// The commit ID of the pull request. This is required for reviews on multi-commit pull requests.
+	CommitID string `json:"commit_id,omitempty"`
+	// The path to the file being commented on. This is required for single-file reviews.
+	Path string `json:"path,omitempty"`
+	// The position in the diff to comment on. This is required for single-file reviews.
+	Position int `json:"position,omitempty"`
+	// The ID of an existing review to update. This is optional.
+	ReviewID int `json:"review_id,omitempty"`
+	// Set to true to submit the review and mark the pull request as reviewed. This is optional.
+	Submit *bool `json:"submit,omitempty"`
+	// Set to true to request changes to the pull request. This is optional.
+	RequestChanges *bool `json:"request_changes,omitempty"`
+}
+
+type ReviewComment struct {
+	// The body of the comment.
+	Body string `json:"body"`
+	// The relative path to the file that this comment applies to.
+	Path string `json:"path"`
+	// The line index in the diff to which the comment applies. Note that this is not a line number in the file itself.
+	Position int `json:"position"`
+}
